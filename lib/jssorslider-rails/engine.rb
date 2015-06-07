03e7ed9129bddb30bca6e11cd8904d9b0fd2bc15
Matthias Grundmann <matthias@glasmail.de>
2015-06-07 17:44:26 +0200

    Add some images to asset precompile

--- lib/jssorslider-rails/engine.rb
+++ lib/jssorslider-rails/engine.rb
@@ -1,6 +1,7 @@
 module JssorsliderRails
   class Engine < ::Rails::Engine
     initializer :assets do |config|
+      Rails.application.config.assets.precompile += %w( a17.png loading.gif loading2.gif )
       Rails.application.config.assets.paths << root.join('vendor', 'assets')
     end
   end
